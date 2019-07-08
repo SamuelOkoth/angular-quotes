66e0eb8648411199f1ce879d7c069e29ca5663d0
SamuelOkoth <okothsamuel78@gmail.com>
2019-07-08 12:43:11 +0300

    updated my quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
     public showDetails:boolean;
     constructor(public id:number, public author:string,public publisher:string,public quoteDesc:string,public date:Date,public likes:number,public dislikes:number){
-    this.showDetails=fa
+    this.showDetails=false
 
 }
 
