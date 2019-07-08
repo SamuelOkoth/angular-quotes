57571f2264cdf78c40b0d580b99c85c5875d0fb5
SamuelOkoth <okothsamuel78@gmail.com>
2019-07-08 11:27:43 +0300

    modified the readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Quotes
 
-First Independent project for Moringa Core , May 25th, 2018
+First Independent project for Moringa Core
 
 ## Description
 A webpage that allows users to enter quotes, and display them back to the user.When a user submits the quotes, the quote appears on the quotes part in the webpage.The author of the quote,date submitted and delete button for the the quote are hidden in the details section. Pressing the details section will reveal such details. Pressing the delete button will delete the entire quote with a confirmation message for from the page. The quote with the highest number of upvotes will be highlighted.
@@ -19,3 +19,4 @@ No Known bugs so far
 Contact me for any comments, reviews or advice.
 
 ##Deployed site link
+https://github.com/SamuelOkoth/Quotes.git
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Quote} from '../quote'
+import { Quote } from '../quote'
 
 
 @Component({
@@ -8,55 +8,55 @@ import {Quote} from '../quote'
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  public maxCount =0;
-  title = "QUOTES BY ANGULAR"
-  quotes =[
-    new Quote(1, 'Mandera','Liz','Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do'
-    ,new Date(2018,5,25,) ,0,0),
-    new Quote(1, '','Liz','Two things are infinite: the universe and human stupidity; and Im not sure about the universe'
-    ,new Date(2018,5,25,) ,0,0),
+  public maxCount = 0;
+  title = "QUOTES"
+  quotes = [
+    new Quote(1, 'Samuel', 'Luther', 'Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do'
+      , new Date(2019, 7, 8, ), 0, 0),
+    new Quote(1, '', 'Einsten', 'Two things are infinite: the universe and human stupidity; and Im not sure about the universe'
+      , new Date(2019, 7, 8, ), 0, 0),
 
   ];
 
 
 
-  submitLike(quote,index){
-    quote.likes=quote.likes+1;
+  submitLike(quote, index) {
+    quote.likes = quote.likes + 1;
     // this.addNewQ(quote);
-    if(quote.likes>this.maxCount) this.maxCount=quote.likes;
-    else if(quote.likes<=this.maxCount) console.log(this.maxCount);
+    if (quote.likes > this.maxCount) this.maxCount = quote.likes;
+    else if (quote.likes <= this.maxCount) console.log(this.maxCount);
   }
 
 
 
 
-  submitUnlike(quote){
-    quote.dislikes= quote.dislikes+1
+  submitUnlike(quote) {
+    quote.dislikes = quote.dislikes + 1
   }
 
 
 
-  addNewQuote(quote){
+  addNewQuote(quote) {
     let quoteLength = this.quotes.length;
-    quote.id = quoteLength+1;
+    quote.id = quoteLength + 1;
     quote.completeDate = new Date(quote.completeDate)
     this.quotes.push(quote);
   }
 
-  completeQuote(isComplete,index){
-    if(isComplete){
+  completeQuote(isComplete, index) {
+    if (isComplete) {
       let toComplete = confirm('Are you sure you want to delete?')
-      if(toComplete){
-        this.quotes.splice(index,1)
+      if (toComplete) {
+        this.quotes.splice(index, 1)
       }
     }
   }
 
 
 
-  toogleDetails(index){
+  toogleDetails(index) {
     this.quotes[index].showDetails = !this.quotes[index].showDetails;
-}
+  }
 
   constructor() { }
 
@@ -64,5 +64,3 @@ export class QuoteComponent implements OnInit {
   }
 
 }
-
-
